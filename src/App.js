f2330990d2284633d9cb627122b7d15ec8f919f3
Vitaliy Galata <vitaliy.galata@tangramltd.com>
2023-08-29 22:40:30 +0300

    use useEffect

--- src/App.js
+++ src/App.js
@@ -1,62 +1,27 @@
-import { useState, useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
-  const [state, setState] = useState({
-    count: 0,
-    isCounting: false,
-  });
-
-  const [value, setValue] = useState(() => {
-    return 10;
-  });
-
-  const [someValue, setSomeValue] = useState(0);
+  let [count, setCount] = useState(0);
 
   useEffect(() => {
-    console.log('someValue', someValue);
-  }, [someValue]);
+    console.log('start');
+  }, [count]);
 
   useEffect(() => {
-    console.log('state', state);
-  }, [state]);
+    const handler = () => {
+      setCount(count + 1);
+      console.log('work');
+    };
 
-  const handleCount = () => {
-    setState({
-      ...state, // все существующие ключи будут скопированы в state
-      count: state.count + 1,
-    });
-  };
+    document.addEventListener('click', handler);
 
-  const handleStatus = () => {
-    setState({
-      ...state,
-      isCounting: !state.isCounting,
-    });
-  };
+    return () => {
+      document.removeEventListener('click', handler);
+    };
+  }, []);
 
-  const cliker = () => {
-    setValue((prevValue) => {
-      return prevValue + 1;
-    });
-  };
-
-  const handleSomeValue = () => {
-    // setSomeValue((prevValue) => {
-    //   return prevValue + 1;
-    // });
-    setSomeValue(someValue + 1);
-  };
-
-  return (
-    <div className="App">
-      <button onClick={handleCount}>Change count</button>
-      <button onClick={handleStatus}>Change status</button>
-      <button onClick={handleSomeValue}>Change somevalue</button>
-      value: {value} | someValue: {someValue}
-      <button onClick={cliker}>Click</button>
-    </div>
-  );
+  return <div className="App">{count}</div>;
 }
 
 export default App;
