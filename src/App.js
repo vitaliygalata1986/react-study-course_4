0c7c43a0570c6bd410b4079b9419c0b89b54a161
Vitaliy Galata <vitaliy.galata@tangramltd.com>
2023-08-30 14:48:30 +0300

    use useLayoutEffect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import './App.css';
-import { Context } from './components/Context';
-import Books from './components/Books';
+import { useState, useEffect, useLayoutEffect } from 'react';
 
 function App() {
-  return (
-    <Context>
-      <Books />
-    </Context>
-  );
+  const [value, setValue] = useState(0);
+
+  useLayoutEffect(() => {
+    if (value === 0) {
+      setValue(10 + Math.random() * 200);
+    }
+  }, [value]);
+
+  console.log('render', value);
+
+  return <button onClick={() => setValue(0)}>value: {value}</button>;
 }
 
 export default App;
