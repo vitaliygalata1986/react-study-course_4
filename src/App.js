278cc8b8e82a0039c2bbea8f41760d6eb33edc24
Vitaliy Galata <vitaliy.galata@tangramltd.com>
2023-09-02 23:23:20 +0300

    use useCallback, useMemo

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import './App.css';
-import { useState, useEffect, useLayoutEffect } from 'react';
+import { useState, useCallback } from 'react';
+import CountButton from './components/CountButton';
 
 function App() {
-  const [value, setValue] = useState(0);
+  const [count1, setCount1] = useState(0);
+  const [count2, setCount2] = useState(0);
+  // const increment1 = () => setCount1((c) => c + 1);
+  const increment1 = useCallback(() => setCount1((c) => c + 1), []);
+  // const increment2 = () => setCount2((c) => c + 1);
+  const increment2 = useCallback(() => setCount2((c) => c + 1), []);
 
-  useLayoutEffect(() => {
-    if (value === 0) {
-      setValue(10 + Math.random() * 200);
-    }
-  }, [value]);
-
-  console.log('render', value);
-
-  return <button onClick={() => setValue(0)}>value: {value}</button>;
+  return (
+    <>
+      <CountButton count={count1} onClick={increment1}></CountButton>
+      <CountButton count={count2} onClick={increment2}></CountButton>
+    </>
+  );
 }
 
 export default App;
